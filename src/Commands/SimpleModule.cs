71e011f2e14278391d7f067a2a0c9ba12c265b4c
Ubuntu <ubuntu@ip-172-31-16-155.us-east-2.compute.internal>
2022-07-06 22:51:56 +0000

    added async functionality

--- src/Commands/SimpleModule.cs
+++ src/Commands/SimpleModule.cs
@@ -5,7 +5,7 @@ using Discord.Commands;
 public class SimpleModule : ModuleBase<SocketCommandContext> {
 	[Command("say")]
 	[Summary("Repeats your message.")]
-	public Task SayAsync([Remainder][Summary("The text to repeat")] string echo) {
+	public async Task SayAsync([Remainder][Summary("The text to repeat")] string echo) {
 		await Context.Message.DeleteAsync();
 		return ReplyAsync(echo);
 	}
@@ -13,13 +13,13 @@ public class SimpleModule : ModuleBase<SocketCommandContext> {
 	protected static Random randy = new Random(69420666);
 	[Command("random")]
 	[Summary("Returns an inclusive number between the lower and upper bound.")]
-	public Task RandomAsync(int lower, int upper) {
+	public async Task RandomAsync(int lower, int upper) {
 		return ReplyAsync(randy.Next(lower, upper).ToString());
 	}
 
 	[Command("time")]
 	[Summary("Returns the current unix timestamp")]
-	public Task TimeAsync() {
+	public async Task TimeAsync() {
 		long time = Context.Message.Timestamp.ToUnixTimeSeconds();
 		return ReplyAsync($"<t:{time}>");
 	}
