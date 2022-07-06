277d1ca8eaf2466e55cab3b6760c7a669f182d58
bforbiggy <AndyHeCS@protonmail.com>
2022-07-06 18:55:59 -0400

    Proper awaits

--- src/Commands/SimpleModule.cs
+++ src/Commands/SimpleModule.cs
@@ -7,20 +7,20 @@ public class SimpleModule : ModuleBase<SocketCommandContext> {
 	[Summary("Repeats your message.")]
 	public async Task SayAsync([Remainder][Summary("The text to repeat")] string echo) {
 		await Context.Message.DeleteAsync();
-		return ReplyAsync(echo);
+		await ReplyAsync(echo);
 	}
 
 	protected static Random randy = new Random(69420666);
 	[Command("random")]
 	[Summary("Returns an inclusive number between the lower and upper bound.")]
 	public async Task RandomAsync(int lower, int upper) {
-		return ReplyAsync(randy.Next(lower, upper).ToString());
+		await ReplyAsync(randy.Next(lower, upper).ToString());
 	}
 
 	[Command("time")]
 	[Summary("Returns the current unix timestamp")]
 	public async Task TimeAsync() {
 		long time = Context.Message.Timestamp.ToUnixTimeSeconds();
-		return ReplyAsync($"<t:{time}>");
+		await ReplyAsync($"<t:{time}>");
 	}
 }
